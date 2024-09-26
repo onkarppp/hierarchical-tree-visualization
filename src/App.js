44aa880b13a5a285d5f5097ec97537036adf495e
onkarppp <115416311+onkarppp@users.noreply.github.com>
2024-09-26 15:42:09 +0530

    Update App.js
    Small css change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./App.css"; // Custom CSS to include styles
+import "./App.css"; 
 
 function App() {
   const [data, setData] = useState([]);
